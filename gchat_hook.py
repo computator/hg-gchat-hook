da051a5aea323515d930f6bbf966a437f8df8222
Reuben Lifshay <rlifshay@gmail.com>
2018-06-05 20:47:06 -0700

    Initial implementation.

---
+++ gchat_hook.py
@@ -0,0 +1,44 @@
+from mercurial import cmdutil, color
+import urllib2
+import json
+
+def push_notify(ui, repo, node, node_last, url, **kwargs):
+	notify_url = ui.config('gchat', 'notify_url')
+	if not notify_url:
+		ui.warn("Skipping push_notify hook because notify_url is not set for gchat\n")
+		return True
+	tpl = ui.config('gchat', 'notify_template', "{count} changesets {action}ed from repository _{url}_:\n\n{log}\n")
+	log_tpl = ui.config('gchat', 'notify_log_template', 'status')
+
+	orig_color = ui.config('ui', 'color')
+	ui.setconfig('ui', 'color', 'off')
+	color.setup(ui)
+
+	revs = repo.revs('{}:{}'.format(node_last, node))
+	disp = cmdutil.show_changeset(ui, repo, {'template': log_tpl})
+	logtext = ''
+	for rev in revs:
+		ui.pushbuffer(labeled=False)
+		disp.show(repo[rev])
+		newtext = ui.popbuffer()
+		if len(logtext) + len(newtext) + len(tpl) > 4000:
+			logtext += "\n*Changeset log truncated...*"
+			break
+		logtext += newtext
+	disp.close()
+
+	ui.setconfig('ui', 'color', orig_color)
+	color.setup(ui)
+
+	args = {
+		'count': len(revs),
+		'action': kwargs['source'],
+		'url': url,
+		'log': logtext
+	}
+	text = tpl.format(**args)
+	if len(text) > 4096:
+		text = text[:4096-16] + "_<truncated...>_"
+
+	req = urllib2.Request(notify_url, json.dumps({'text': text}), {'Content-Type': 'application/json'})
+	urllib2.urlopen(req, timeout=10)
